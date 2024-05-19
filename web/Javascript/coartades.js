805d3ffd37d5f0ba16ea0edc2956472ca9581174
Alba <2023_alba.segura@iticbcn.com>
2024-05-19 21:19:29 +0200

    corregir faltes

--- web/Javascript/coartades.js
+++ web/Javascript/coartades.js
@@ -9,13 +9,13 @@ logRequest("http://localhost:3005/api/coartades", 'GET');
 fetch("http://localhost:3005/api/coartades")
     .then(response => response.json())
     .then(data => {
-        console.log('Raw data:', data); // Log the raw data
+        console.log('Raw data:', data);
         const names = data.filter(coartades => coartades  !== null).map(coartades  => {
             return coartades.nom + ": " + coartades.text+ "<br>";
         })
         console.log(names);
         console.log(names.join(", "));
-        // Display the data
+        //  CGLLM
         const apiDataDiv = document.getElementById('api-datacoartades');
         apiDataDiv.innerHTML = names.join("<br>","<br>");
     })
